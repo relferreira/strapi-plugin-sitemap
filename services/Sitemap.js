61ce8e0977b45cc094735ed7cef67e0f844b48b7
Renan Ferreira <relferreira@gmail.com>
2022-05-25 17:44:53 -0300

    Testing new slug field

--- services/Sitemap.js
+++ services/Sitemap.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-const { Map } = require('immutable');
-const { SitemapStream, streamToPromise } = require('sitemap');
-const { isEmpty, trim } = require('lodash');
-const fs = require('fs');
+const { Map } = require("immutable");
+const { SitemapStream, streamToPromise } = require("sitemap");
+const { isEmpty, trim } = require("lodash");
+const fs = require("fs");
 
 /**
  * Sitemap.js service
@@ -13,42 +13,42 @@ const fs = require('fs');
 
 const createDefaultConfig = async () => {
   const pluginStore = strapi.store({
-    environment: '',
-    type: 'plugin',
-    name: 'sitemap',
+    environment: "",
+    type: "plugin",
+    name: "sitemap",
   });
 
   const value = {
-    hostname: '',
+    hostname: "",
     includeHomepage: true,
     excludeDrafts: true,
     contentTypes: Map({}),
     customEntries: Map({}),
-  }
+  };
 
-  await pluginStore.set({ key: 'settings', value });
+  await pluginStore.set({ key: "settings", value });
 
   return await strapi
     .store({
-      environment: '',
-      type: 'plugin',
-      name: 'sitemap',
+      environment: "",
+      type: "plugin",
+      name: "sitemap",
     })
-    .get({ key: 'settings' });
+    .get({ key: "settings" });
 };
 
 module.exports = {
   getConfig: async () => {
     let config = await strapi
       .store({
-        environment: '',
-        type: 'plugin',
-        name: 'sitemap',
+        environment: "",
+        type: "plugin",
+        name: "sitemap",
       })
-      .get({ key: 'settings' });
+      .get({ key: "settings" });
 
     if (!config) {
-      config = await createDefaultConfig('');
+      config = await createDefaultConfig("");
     }
 
     if (!config.customEntries) {
@@ -62,27 +62,27 @@ module.exports = {
     const config = await module.exports.getConfig();
     let contentTypes = {};
 
-    Object.values(strapi.contentTypes).map(contentType => {
+    Object.values(strapi.contentTypes).map((contentType) => {
       let uidFieldName = false;
 
       Object.entries(contentType.__schema__.attributes).map(([i, e]) => {
-        if (e.type === "uid") {
+        if (e.type === "uid" || e.type === "string") {
           uidFieldName = i;
         }
-      })
+      });
 
       if (uidFieldName) {
         contentTypes[contentType.modelName] = {
           uidField: uidFieldName,
           priority: 0.5,
-          changefreq: 'monthly',
-          area: ''
+          changefreq: "monthly",
+          area: "",
         };
       }
-    })
+    });
 
     return {
-      hostname: '',
+      hostname: "",
       customEntries: config.customEntries,
       contentTypes,
     };
@@ -98,12 +98,12 @@ module.exports = {
 
       Object.entries(e).map(([i, e]) => {
         if (i === config.contentTypes[contentType].uidField) {
-          const area = trim(config.contentTypes[contentType].area, '/');
-          const url = [area, e].filter(Boolean).join('/')
+          const area = trim(config.contentTypes[contentType].area, "/");
+          const url = [area, e].filter(Boolean).join("/");
           pageData[id].url = url;
         }
-      })
-    })
+      });
+    });
 
     return pageData;
   },
@@ -112,57 +112,69 @@ module.exports = {
     const config = await module.exports.getConfig();
     const sitemapEntries = [];
 
-    await Promise.all(Object.keys(config.contentTypes).map(async (contentType) => {
-      let modelName;
-      const contentTypeByName = Object.values(strapi.contentTypes)
-        .find(strapiContentType => strapiContentType.info.name === contentType);
+    await Promise.all(
+      Object.keys(config.contentTypes).map(async (contentType) => {
+        let modelName;
+        const contentTypeByName = Object.values(strapi.contentTypes).find(
+          (strapiContentType) => strapiContentType.info.name === contentType
+        );
 
-      // Backward compatibility for issue https://github.com/boazpoolman/strapi-plugin-sitemap/issues/4
-      if (contentTypeByName) {
-        modelName = contentTypeByName.modelName;
-      } else {
-        modelName = contentType;
-      }
+        // Backward compatibility for issue https://github.com/boazpoolman/strapi-plugin-sitemap/issues/4
+        if (contentTypeByName) {
+          modelName = contentTypeByName.modelName;
+        } else {
+          modelName = contentType;
+        }
 
-      const hasDraftAndPublish = strapi.query(modelName).model.__schema__.options.draftAndPublish;
-      let pages = await strapi.query(modelName).find({_limit: -1});
+        const hasDraftAndPublish =
+          strapi.query(modelName).model.__schema__.options.draftAndPublish;
+        let pages = await strapi.query(modelName).find({ _limit: -1 });
 
-      if (config.excludeDrafts && hasDraftAndPublish) {
-        pages = pages.filter((page) => page.published_at);
-      }
+        if (config.excludeDrafts && hasDraftAndPublish) {
+          pages = pages.filter((page) => page.published_at);
+        }
 
-      const pageData = await module.exports.getSitemapPageData(contentType, pages, config);
+        const pageData = await module.exports.getSitemapPageData(
+          contentType,
+          pages,
+          config
+        );
 
-      Object.values(pageData).map(({ url, lastmod }) => {
-        sitemapEntries.push({
-          url,
-          lastmod,
-          changefreq: config.contentTypes[contentType].changefreq,
-          priority: config.contentTypes[contentType].priority,
-        })
+        Object.values(pageData).map(({ url, lastmod }) => {
+          sitemapEntries.push({
+            url,
+            lastmod,
+            changefreq: config.contentTypes[contentType].changefreq,
+            priority: config.contentTypes[contentType].priority,
+          });
+        });
       })
-    }));
+    );
 
     if (config.customEntries) {
-      await Promise.all(Object.keys(config.customEntries).map(async (customEntry) => {
-        sitemapEntries.push({
-          url: customEntry,
-          changefreq: config.customEntries[customEntry].changefreq,
-          priority: config.customEntries[customEntry].priority,
+      await Promise.all(
+        Object.keys(config.customEntries).map(async (customEntry) => {
+          sitemapEntries.push({
+            url: customEntry,
+            changefreq: config.customEntries[customEntry].changefreq,
+            priority: config.customEntries[customEntry].priority,
+          });
         })
-      }));
+      );
     }
 
     // Add a homepage when none is present
     if (config.includeHomepage) {
-      const hasHomePage = !isEmpty(sitemapEntries.filter(entry => entry.url === ''));
+      const hasHomePage = !isEmpty(
+        sitemapEntries.filter((entry) => entry.url === "")
+      );
 
       if (!hasHomePage) {
         sitemapEntries.push({
-          url: '/',
-          changefreq: 'monthly',
-          priority: '1',
-        })
+          url: "/",
+          changefreq: "monthly",
+          priority: "1",
+        });
       }
     }
 
@@ -176,21 +188,22 @@ module.exports = {
           if (err) throw err;
         });
       })
-      .catch(() => console.error );
+      .catch(() => console.error);
   },
 
   createSitemap: async (sitemapEntries) => {
     const config = await module.exports.getConfig();
     const sitemap = new SitemapStream({ hostname: config.hostname });
 
-    const allSitemapEntries = sitemapEntries || await module.exports.createSitemapEntries();
+    const allSitemapEntries =
+      sitemapEntries || (await module.exports.createSitemapEntries());
 
     allSitemapEntries.map((sitemapEntry) => {
-      sitemap.write(sitemapEntry)
-    })
+      sitemap.write(sitemapEntry);
+    });
 
     sitemap.end();
 
-    await module.exports.writeSitemapFile('sitemap.xml', sitemap);
+    await module.exports.writeSitemapFile("sitemap.xml", sitemap);
   },
 };
